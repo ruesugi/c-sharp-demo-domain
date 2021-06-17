59ba48402e9e82e534b225e520e5d1f9866e2ab6
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-06-17 15:14:08 +0900

    change:Form1.csをUserSaveView.csに変更.

--- c-sharp-demo-domain.WinForm/UserSaveView.cs
+++ c-sharp-demo-domain.WinForm/UserSaveView.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace c_sharp_demo.WinForm
 {
-    public partial class Form1 : Form
+    public partial class UserSaveView : Form
     {
-        public Form1()
+        public UserSaveView()
         {
             InitializeComponent();
         }
