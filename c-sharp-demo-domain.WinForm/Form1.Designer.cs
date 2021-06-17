75e525d0869965b73b6190913d5c5dab7769ab44
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-06-17 14:52:21 +0900

    change:画面サイズを300x350に変更.

--- c-sharp-demo-domain.WinForm/Form1.Designer.cs
+++ c-sharp-demo-domain.WinForm/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace c_sharp_demo.WinForm
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(284, 311);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
