ecedf39bc73910f88ad701924d5b2e828408ff2b
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-07-06 16:53:54 +0900

    add:初期値のテストが通る実装を追加.

--- c-sharp-demo-domain.WinForm/ViewModels/UserSaveViewModel.cs
+++ c-sharp-demo-domain.WinForm/ViewModels/UserSaveViewModel.cs
@@ -3,17 +3,36 @@
     public class UserSaveViewModel : ViewModelBase
     {
         private bool _mailCheckBoxChecked = false;
-        public bool MailCheckBoxChecked {
+        public bool MailCheckBoxChecked
+        {
             get { return _mailCheckBoxChecked; }
             set
             {
                 SetProperty(ref _mailCheckBoxChecked, value);
-            } 
+            }
         }
         public bool MailAddressLabelEnabled { get; set; }
         public bool MailAddressTextBoxEnabled { get; set; }
-        public object FreeRadioButtonChecked { get; set; }
-        public object BusinessRadioButtonChecked { get; set; }
+
+        private bool _freeRadioButtonChecked = false;
+        public bool FreeRadioButtonChecked
+        {
+            get { return _freeRadioButtonChecked; }
+            set
+            {
+                SetProperty(ref _freeRadioButtonChecked, value);
+            }
+        }
+
+        private bool _businnesRadioButtonChecked = false;
+        public bool BusinessRadioButtonChecked
+        {
+            get { return _businnesRadioButtonChecked; }
+            set
+            {
+                SetProperty(ref _businnesRadioButtonChecked, value);
+            }
+        }
 
         public void ChangeMailAddressEnabled()
         {
