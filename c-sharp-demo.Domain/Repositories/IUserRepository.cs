9c25645d3e46d3ac6d57c1b90683b5193323679f
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-07-13 15:50:16 +0900

    add:ユーザー情報にかかわるインターフェイスを定義するIUserRepositoryを追加した.

---
+++ c-sharp-demo.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using c_sharp_demo.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_demo.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        void Save(UserEntity userEntity);
+    }
+}
