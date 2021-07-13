1b968448daaaa6791d015702ba4eb3d8cfe452e7
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-07-13 15:52:51 +0900

    add:ユーザー情報を外部のCsvファイルでやり取りするためのUserCsvクラスを追加.

---
+++ c-sharp-demo.Infrastructure/Csv/UserCsv.cs
@@ -0,0 +1,18 @@
+﻿using c_sharp_demo.Domain.Entities;
+using c_sharp_demo.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_demo.Infrastructure.Csv
+{
+    public class UserCsv : IUserRepository
+    {
+        public void Save(UserEntity userEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
