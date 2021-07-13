3d7cbd183ebd8ec8d87c677d876c40876ff61909
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-07-13 15:43:40 +0900

    add:料金プランにかんするValueObjectを追加.

---
+++ c-sharp-demo.Domain/ValueObects/PricePlan.cs
@@ -0,0 +1,16 @@
+﻿namespace c_sharp_demo.Domain.ValueObects
+{
+    public sealed class PricePlan : ValueObject<PricePlan>
+    {
+        public PricePlan(int value)
+        {
+            Value = value;
+        }
+        public int Value { get; }
+
+        protected override bool EqualsCore(PricePlan other)
+        {
+            return Value == other.Value;
+        }
+    }
+}
