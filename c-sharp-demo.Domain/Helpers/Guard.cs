9193d80ba6f9c3c300c887c86d8a5e924c786cf6
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-07-15 11:06:05 +0900

    add:ガード処理を記述するGuardクラスを追加した.

---
+++ c-sharp-demo.Domain/Helpers/Guard.cs
@@ -0,0 +1,21 @@
+﻿using c_sharp_demo.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace c_sharp_demo.Domain.Helpers
+{
+    public static class Guard
+    {
+        public static void IsNumber(string text, string message)
+        {
+            if (!Regex.IsMatch(text, @"^[0-9]+$"))
+            {
+                throw new InputException(message);
+            }
+        }
+    }
+}
