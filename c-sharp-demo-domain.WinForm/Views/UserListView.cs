dba5fef21700ad252e62dfe581c9e4fb592e0bb4
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-07-15 14:12:20 +0900

    add:DataGridViewとUsersをバインディングした.

--- c-sharp-demo-domain.WinForm/Views/UserListView.cs
+++ c-sharp-demo-domain.WinForm/Views/UserListView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using c_sharp_demo_domain.WinForm.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,15 @@ namespace c_sharp_demo_domain.WinForm.Views
 {
     public partial class UserListView : Form
     {
+        private readonly UserListViewModel _viewModel = new UserListViewModel();
         public UserListView()
         {
             InitializeComponent();
+
+            UserDataGrid.DataBindings.Add(
+                "DataSource",
+                _viewModel,
+                nameof(_viewModel.Users));
         }
     }
 }
